85c1ebda0b7ddb8013fb3c6ad4abb47ec7f47fcc
Sundari-1004 <sundari.mathi.sundaram@gmail.com>
2024-03-27 15:28:24 +0530

    Updated by Mathi

--- src/com/gitdir/GitDemo.java
+++ src/com/gitdir/GitDemo.java
@@ -4,7 +4,12 @@ public class GitDemo {
 	public static void main(String[] args)
 	{
 		System.out.println("Hi this is a Demo ");
+		GitDemo g= new GitDemo();
+		g.login();
 
 	}
-
+	private void login()
+	{
+		System.out.println("This is a demo by Mathi");
+	}
 }
